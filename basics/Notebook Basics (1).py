3973326c4e7da56e29ca580689ce767ab5870bd5
rahul.jaiswal8390 <rahul.jaiswal8390>
2023-05-26 07:11:16 +0000

    new notebook

---
+++ basics/Notebook Basics (1).py
@@ -0,0 +1,46 @@
+# Databricks notebook source
+print("Hello")
+
+# COMMAND ----------
+
+select "rahul"
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select "rahul"
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Hi
+# MAGIC ## hi
+# MAGIC ### hi
+
+# COMMAND ----------
+
+# MAGIC %run ./setup
+
+# COMMAND ----------
+
+print(v_full_name)
+
+# COMMAND ----------
+
+# MAGIC %fs ls 'databricks-datasets'
+
+# COMMAND ----------
+
+dbutils.help()
+
+# COMMAND ----------
+
+dbutils.fs.help()
+
+# COMMAND ----------
+
+dbu
+
+# COMMAND ----------
+
+
